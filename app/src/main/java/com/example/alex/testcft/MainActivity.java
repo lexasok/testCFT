7662b0c514a8dcd58b37a96fc1fefbd37895f8e4
alex <sokolov@ozero.net>
2018-08-29 19:59:06 +0300

    Colors changed

--- app/src/main/java/com/example/alex/testcft/MainActivity.java
+++ app/src/main/java/com/example/alex/testcft/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
